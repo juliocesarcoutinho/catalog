fe0fbff3f4422918068e937f50efa3ad95ca1436
Julio Cesar Coutinho <juliocesar.coutinho@outlook.com>
2024-04-16 22:29:05 -0300

    Criação e configurações da entity category

---
+++ backend/src/main/resources/data.sql
@@ -0,0 +1,11 @@
+insert into tb_category (name)
+values ('Electronics'),
+       ('Books'),
+       ('Home & Kitchen'),
+       ('Fashion'),
+       ('Toys & Games'),
+       ('Beauty'),
+       ('Sports & Outdoors'),
+       ('Automotive'),
+       ('Health & Personal Care'),
+       ('Baby Products');
---
+++ backend/src/main/resources/db/migration/V001__criação-tabela-category.sql
@@ -0,0 +1,4 @@
+CREATE TABLE tb_category (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL
+);
